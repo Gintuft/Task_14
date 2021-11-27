281fb51f6538a1fea7abfd8d9d3c0ad36ec4626e
Maria Gintuft <marvalvlad@gmail.com>
2021-11-27 19:14:53 +0300

    Create class TodoFormCreate

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-class TodoElement{
-    constructor (todo){
-        
+let data = []
+const listElement = document.querySelector('#list')
+const formCreateElement = document.querySelector('#formCreate')
 
-    }
+class TodoFormCreate{
+  constructor (formCreateElement){
+    this.formCreateElement = formCreateElement
+    this.init()
+  }
+  init(){
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+    this.formCreateElement.addEventListener('submit', this.handleSubmit)
+  }
+
+handleSubmit (event) {
+  event.preventDefault()
+
+  const toDo = {
+    id: new Date().getTime(),
+    isChecked: false
+  }
+
+  const formData = new FormData(formCreateElement)
+  for (let [name, value] of formData.entries()) {
+    toDo[name] = value
+  }
+
+  data.push(toDo)
+  this.formCreateElement.reset()
+
+  render()
 }
+}
+
+new TodoFormCreate(formCreateElement)
